ade172e5a04eaaa642300ea8ad7bae2b98901d93
Erwin Kroon <erwin+git@k3n.nl>
2015-12-07 00:59:47 +0100

    WAAA

--- playlist-viewer/src/playlist_viewer/core.cljs
+++ playlist-viewer/src/playlist_viewer/core.cljs
@@ -1,8 +1,12 @@
 (ns playlist-viewer.core
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require
     [reagent.core :as reagent :refer [atom]]
     [playlist-viewer.global :refer [app-state]]
-    [playlist-viewer.views :refer [container render-playlist render-all-playlists]]))
+    [playlist-viewer.views :refer [container render-playlist render-all-playlists]]
+    [cljs-http.client :as http]
+    [cljs-http.util :as util]
+    [cljs.core.async :refer [timeout <!]]))
 
 (enable-console-print!)
 (println "Edits to this text should show up in your developer console.")
@@ -22,4 +26,9 @@
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  (go-loop []
+    (let [response (<! (http/get "http://nodejsplz.monkeyregal.com/last"))
+          decoded  (util/json-decode response)]
+      (swap! app-state (fn [prev] (update-in prev [:playlists 0 :tracks] decoded)))
+      (<! (timeout 15000)))
+    (recur)))
--- playlist-viewer/src/playlist_viewer/global.cljs
+++ playlist-viewer/src/playlist_viewer/global.cljs
@@ -20,49 +20,8 @@
                      {:current-playlist 0
                       :playlists
                         [ {
-                           :name "My Jazz Favourites"
-                           :created "2015-12-06"
+                           :name "All"
                            :tracks [
-                            {:name "Track 1"
-                             :artist "Dizzy Gillespie"
-                             :album "It's all Jazz"
-                             :meta {}}
-                            {:name "Track 2"
-                             :artist "Dizzy Gillespie"
-                             :album "It's all Jazz"
-                             :meta {}}
-                            {:name "Track 3"
-                             :artist "Dizzy Gillespie"
-                             :album "It's all Jazz"
-                             :meta {}}
-                            {:name "Track 4"
-                             :artist "Dizzy Gillespie"
-                             :album "It's all Jazz"
-                             :meta {}}
-                            {:name "Track 5"
-                             :artist "Dizzy Gillespie"
-                             :album "It's all Jazz"
-                             :meta {}}
-                            {:name "Track 6"
-                             :artist "Dizzy Gillespie"
-                             :album "It's all Jazz"
-                             :meta {}}
-                            {:name "Track 7"
-                             :artist "Dizzy Gillespie"
-                             :album "It's all Jazz"
-                             :meta {}}
-                            {:name "Track 8"
-                             :artist "Dizzy Gillespie"
-                             :album "It's all Jazz"
-                             :meta {}}
-                            {:name "Track 9"
-                             :artist "Dizzy Gillespie"
-                             :album "It's all Jazz"
-                             :meta {}}
-                            {:name "Track 10"
-                             :artist "Dizzy Gillespie"
-                             :album "It's all Jazz"
-                             :meta {}}
                                     ]
                            }
                          {:name "Second Playlist"
