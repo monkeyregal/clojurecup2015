6ed0d4cc12762290e01521cb303d75cda1471846
Raymond Kroon <raymond@k3n.nl>
2015-12-06 11:51:49 +0100

    fix: setup script (for linux)

--- dev/gracenotesdk.sh
+++ dev/gracenotesdk.sh
@@ -5,9 +5,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR
 if ls gnsdk-*.zip; then
     find ./ -name 'gnsdk-*.zip' -exec unzip {} -d gnsdk \;
-    cp -R gnsdk/lib/* ../streamparser/vendor/lib/
     mkdir -p ../streamparser/vendor/lib
     mkdir -p ../streamparser/vendor/jar
+    cp -R gnsdk/lib/* ../streamparser/vendor/lib/
     cp -R gnsdk/wrappers/gnsdk_java/lib/* ../streamparser/vendor/lib/
     cp -R gnsdk/wrappers/gnsdk_java/jar/* ../streamparser/vendor/jar/
     mvn install:install-file \
--- streamparser/setupEnv.sh
+++ streamparser/setupEnv.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
 
+source settings.sh
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export GNSDK_ARCH="mac_x86-64"
 export GNSDK_JAVA="j2se"
 export GNSDK_LIB="$(pwd)/vendor/lib/$GNSDK_ARCH"
 #export GNSDK_JAR="$(pwd)/vendor/jar/$GNSDK_JAVA"
@@ -12,5 +13,3 @@ export GNSDK_LIB="$(pwd)/vendor/lib/$GNSDK_ARCH"
 #export BOOT_JVM_OPTIONS="-client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xmx2g -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Xverify:none -Djava.library.path=$GNSDK_WRAPPER_LIB"
 
 export BOOT_JVM_OPTIONS="-Djava.library.path=$GNSDK_LIB"
-
-source settings.sh
